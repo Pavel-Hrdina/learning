b6c69b51f1d7947b21c9b7b876bd105dc31ceae1
pavel <pavel.hrdina.ml@seznam.cz>
2023-01-05 18:12:37 +0100

    refractoring docs 📝

--- go/docs/docs/.vuepress/.temp/internal/pagesComponents.js
+++
@@ -1,9 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export const pagesComponents = {
-  // path: /404.html
-  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"/home/pavel/code/learning/go/docs/docs/.vuepress/.temp/pages/404.html.vue")),
-}
-h: /404.html
-  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"/home/pavel/code/learning/go/docs/docs/.vuepress/.temp/pages/404.html.vue")),
-}
