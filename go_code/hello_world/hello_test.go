9071fa6c249505eae3a88b4b0889c4469436b70f
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2023-01-29 18:04:12 +0100

    test(hello_test.go): change test for hello.go

--- go_code/hello_world/hello_test.go
+++ go_code/hello_world/hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func test_hello(t *testing.T) {
-	got := hello()
-	want := "Hello, world"
+	t.Run("greet people", func(t *testing.T) {
+		got := hello("Joe")
+		want := "Hello, Joe"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("say, 'Hello, World' when empty string is given", func(t *testing.T) {
+		got := hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
